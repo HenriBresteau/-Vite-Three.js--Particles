cbb0825c276721829625d8dfbaaf23f95abb537c
henriB <henri.bretseau@gmail.com>
2021-03-09 16:53:57 +0100

    circle

--- main.js
+++ main.js
@@ -1,16 +1,26 @@
 import {
   AxesHelper,
   BoxBufferGeometry,
+  BufferGeometry,
+  Float32BufferAttribute,
+  MathUtils,
   Mesh,
   MeshNormalMaterial,
   PerspectiveCamera,
+  Points,
+  PointsMaterial,
   Scene,
+  TextureLoader,
   WebGLRenderer,
 } from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import "./style.css";
 
+const textureLoader = new TextureLoader();
+const circletTexture = textureLoader.load("./circle.png");
 const scene = new Scene();
+const count = 100;
+const distance = 2;
 
 scene.add(new AxesHelper());
 const camera = new PerspectiveCamera(
@@ -24,8 +34,21 @@ camera.position.y = 0.5;
 camera.position.x = 0.5;
 scene.add(camera);
 
-const cube = new Mesh(new BoxBufferGeometry(1, 1, 1), new MeshNormalMaterial());
-scene.add(cube);
+const points = new Float32Array(count * 3);
+for (let index = 0; index < points.length; index++) {
+  points[index] = MathUtils.randFloatSpread(distance * 2);
+}
+const geometry = new BufferGeometry();
+geometry.setAttribute("position", new Float32BufferAttribute(points, 3));
+const pointMaterail = new PointsMaterial({
+  color: 0xff0000,
+  size: 0.5,
+  map: circletTexture,
+  alphaTest: 0.01,
+  transparent:true,
+});
+const pointsObject = new Points(geometry, pointMaterail);
+scene.add(pointsObject);
 
 const renderer = new WebGLRenderer({
   antialias: true,
